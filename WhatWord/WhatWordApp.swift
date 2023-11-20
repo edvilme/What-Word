0a11f90e175ec52973f08036920b6df9c42d1a36
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2023-11-19 20:06:36 -0600

    Initial Commit

---
+++ WhatWord/WhatWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWordApp.swift
+//  WhatWord
+//
+//  Created by Eduardo  Villalpando  on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
